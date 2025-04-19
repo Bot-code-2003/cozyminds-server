dcd77065184db26fc763c9acc08e40c3d05dca8d
Dharmadeep <dharmadeepmadisetty@gmail.com>
2025-04-19 23:54:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js
 import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -6,25 +7,38 @@ import journalRoutes from "./routes/journalRoutes.js";
 import mailRoutes from "./routes/mailRoutes.js";
 import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
+dotenv.config();
 
+const mongoURL = process.env.MONGODB_URL;
 app.use(cors({ origin: "https://cozyminds.vercel.app" }));
+
+// Handle preflight requests
 app.options("*", cors());
+
+// const mongoURL = "mongodb://localhost:27017/CozyMind";
+// app.use(cors());
+
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-
+// Root route
 app.get("/", (req, res) => {
   res.send("Hello from Cozy Minds!");
 });
 
-app.use("/user", userRoutes);
-app.use("/journal", journalRoutes);
-app.use("/mail", mailRoutes);
+// Use routers
+app.use("/user", userRoutes); // All user routes under /api
+app.use("/journal", journalRoutes); // All journal routes under /api
+app.use("/mail", mailRoutes); // Add mail routes
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connect to MongoDB and start the server
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => console.log(error));
 
-export default app; // ✅ No app.listen()
+// export const handler = serverless(app);
